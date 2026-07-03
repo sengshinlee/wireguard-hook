4867e97fe784cc123c7534bc330941bca7b5d95b
Sengshin Lee <sengshinlee@gmail.com>
2026-07-02 22:47:35 -0700

    Add a new folder hooks, a hooks/routes-up.bat and a hooks/routes-down.bat to implement IP splitting (IPv4-only)

---
+++ hooks/routes-down.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+CD /D %~dp0
+
+rundll32.exe ..\dll\cmroute.dll,SetRoutes ^
+             /STATIC_FILE_NAME ..\chnroutes\delete.txt ^
+             /DONT_REQUIRE_URL ^
+             /IPHLPAPI_ACCESS_DENIED_OK
